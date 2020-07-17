31bea9e792d4b9de8e51138bf52d0f06092c2267
iluvcse <61859230+iluvcse@users.noreply.github.com>
2020-07-17 14:21:33 +0530

    modified 4-2

--- ass11.js
+++ ass11.js
@@ -1,4 +1,3 @@
 let os = prompt("What's your os and version?");
-let res = os.split(/[\s]+/);
-console.log("The OS name is "+os.split(" ",1)+" and version is "+res[res.length-1]);
-
+let res = os.split(" ");
+console.log("The os is "+res[0]+" and the version is "+res[1]);
