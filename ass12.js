8d2a0d9b903ddc6289b3a96a861b11730d4e566c
iluvcse <61859230+iluvcse@users.noreply.github.com>
2020-07-17 12:51:22 +0530

    assignmnet 3-3

---
+++ ass12.js
@@ -0,0 +1,8 @@
+let marks = Number(prompt("Enter Marks:"));
+if(marks>=90) console.log("Grade is A");
+else if(marks>=80 && marks<90)  console.log("Grade is B");
+else if(marks>=70 && marks<80)  console.log("Grade is C");
+else if(marks>=60 && marks<70)  console.log("Grade is D");
+else if(marks>=50 && marks<60)  console.log("Grade is E");
+else console.log("Fail");
+
