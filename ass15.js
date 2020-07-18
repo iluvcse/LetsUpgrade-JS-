3353fe85fb0f5adf082cef1f59827a4ca37e7a87
iluvcse <61859230+iluvcse@users.noreply.github.com>
2020-07-18 14:07:44 +0530

    Assignment 5-3

---
+++ ass15.js
@@ -0,0 +1,4 @@
+let shoppingList = ['Chocolates','Biscuits','Honey','Ice-Creams'];
+let shoppingBasket = ["Butter","Milk",...shoppingList,"Ghee"];
+console.log(shoppingList);
+console.log(shoppingBasket);
